fbd2b67a1f9cebccfc524bf4a2ef74d792e29016
perry <priyanshu4contacts@gmail.com>
2020-11-10 20:20:22 +0530

    change from pycharm1 msg

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("githubchange")
     print("pycharmchange")
-    print("pycharm2")
+    print("pycharm22")
 
 
 # Press the green button in the gutter to run the script.
